5d0bc445525acdb20a00bc7d3b2cdf121c792d9f
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-09-16 17:30:55 +0200

    Update Acid
    Fix Acid doesn't make damages

--- src/main/java/fr/iamacat/dangerzone_iamacatfr/items/Acid.java
+++ src/main/java/fr/iamacat/dangerzone_iamacatfr/items/Acid.java
@@ -1,7 +1,9 @@
 package fr.iamacat.dangerzone_iamacatfr.items;
 
 import fr.iamacat.dangerzone_iamacatfr.entities.entity.AcidEntityInstance;
+import fr.iamacat.dangerzone_iamacatfr.entities.entity.LaserBallInstance;
 import fr.iamacat.dangerzone_iamacatfr.util.Tags;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,15 +18,16 @@ public class Acid extends Item {
         this.setTextureName(Tags.MODID +":acid_item");
     }
 
-    public ItemStack onItemRightClick(final ItemStack itemStack, final World world,
+    public ItemStack onItemRightClick(final ItemStack stack, final World world,
                                       final EntityPlayer entityPlayer) {
         if (!entityPlayer.capabilities.isCreativeMode) {
-            --itemStack.stackSize;
+            --stack.stackSize;
         }
         world.playSoundAtEntity(entityPlayer, "random.bow", 3.0f, 1.0f);
         if (!world.isRemote) {
-            world.spawnEntityInWorld(new AcidEntityInstance(world,entityPlayer));
+            world.spawnEntityInWorld(new AcidEntityInstance(world, entityPlayer));
         }
-        return itemStack;
+        return stack;
     }
+
 }

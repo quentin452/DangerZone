c673071b283a181c80a1304687aa3af7e4384f66
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-09-17 18:28:38 +0200

    Update Basilik

--- src/main/java/fr/iamacat/dangerzone_iamacatfr/entities/entity/BasilikInstance.java
+++ src/main/java/fr/iamacat/dangerzone_iamacatfr/entities/entity/BasilikInstance.java
@@ -1,6 +1,4 @@
-//Deobfuscated with https://github.com/SimplyProgrammer/Minecraft-Deobfuscator3000 using mappings "C:\Users\Administrator\Downloads\Minecraft-Deobfuscator3000-1.2.3\Minecraft-Deobfuscator3000-1.2.3\1.7.10 stable mappings"!
 
-//Decompiled by Procyon!
 
 package fr.iamacat.dangerzone_iamacatfr.entities.entity;
 

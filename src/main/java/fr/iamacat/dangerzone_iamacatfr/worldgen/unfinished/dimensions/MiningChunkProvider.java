7aa448cd1089835ea4da74c4932dd4080f4190f9
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-09-19 05:32:22 +0200

    Udpate Dimensions

--- src/main/java/fr/iamacat/dangerzone_iamacatfr/worldgen/unfinished/dimensions/MiningChunkProvider.java
+++ src/main/java/fr/iamacat/dangerzone_iamacatfr/worldgen/unfinished/dimensions/MiningChunkProvider.java
@@ -6,8 +6,10 @@ import net.minecraft.init.Blocks;
 import net.minecraft.util.IProgressUpdate;
 import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderGenerate;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,76 +17,35 @@ import java.util.Random;
 
 public class MiningChunkProvider implements IChunkProvider {
 
-    private World worldObj;
-    private Random random;
+    private final World world;
+    private final long seed;
+    private final ChunkProviderGenerate generateProvider;
 
     public MiningChunkProvider(World world, long seed) {
-        this.worldObj = world;
-        this.random = new Random(seed);
+        this.world = world;
+        this.seed = seed;
+        this.generateProvider = new ChunkProviderGenerate(world, seed, false);
     }
 
     @Override
-    public boolean chunkExists(int x, int z) {
-        return true;
+    public boolean chunkExists(int p_73149_1_, int p_73149_2_) {
+        return false;
     }
 
     @Override
     public Chunk provideChunk(int x, int z) {
-        Chunk chunk = new Chunk(worldObj, x, z);
-
-        generateTerrain(chunk);
-
-        return chunk;
+        return this.generateProvider.provideChunk(x, z);
     }
 
     @Override
     public Chunk loadChunk(int x, int z) {
-        // Votre logique de chargement de chunk va ici
-        // Vous devez retourner le chunk chargé ou généré
-        // Si le chunk n'existe pas, vous pouvez générer un nouveau chunk
-
-        // Exemple de génération d'un nouveau chunk :
-        Chunk chunk = this.provideChunk(x, z); // Utilisez votre méthode provideChunk
-
-        // Si vous avez une sauvegarde du chunk, chargez-la ici
-        // Sinon, générez le contenu du chunk
-
-        return chunk;
+        return this.generateProvider.loadChunk(x, z);
     }
-
-    private void generateTerrain(Chunk chunk) {
-        int chunkX = chunk.xPosition * 16;
-        int chunkZ = chunk.zPosition * 16;
-
-        for (int x = 0; x < 16; x++) {
-            for (int z = 0; z < 16; z++) {
-                int worldX = chunkX + x;
-                int worldZ = chunkZ + z;
-
-                // Configure these values to control terrain generation
-                double terrainScale = 0.1;
-                double noiseValue = random.nextDouble(); // Use Perlin noise or another noise function here
-
-                int seaLevel = 63;
-                int terrainHeight = (int) (noiseValue * 20 + seaLevel);
-
-                for (int y = 0; y < 256; y++) {
-                    Block block = Blocks.air;
-
-                    if (y == terrainHeight || (y < terrainHeight && y > seaLevel)) {
-                        block = Blocks.stone; // Adjust this to your desired terrain block
-                    }
-
-                    chunk.func_150807_a(x, y, z, block, 0);
-                }
-            }
-        }
-    }
-
-    // Implement other methods as needed
 
     @Override
-    public void populate(IChunkProvider provider, int x, int z) {}
+    public void populate(IChunkProvider provider, int x, int z) {
+        this.generateProvider.populate(provider, x, z);
+    }
 
     @Override
     public boolean saveChunks(boolean p_73151_1_, IProgressUpdate p_73151_2_) {
@@ -113,8 +74,7 @@ public class MiningChunkProvider implements IChunkProvider {
     }
 
     @Override
-    public ChunkPosition func_147416_a(World p_147416_1_, String p_147416_2_, int p_147416_3_, int p_147416_4_,
-        int p_147416_5_) {
+    public ChunkPosition func_147416_a(World p_147416_1_, String p_147416_2_, int p_147416_3_, int p_147416_4_, int p_147416_5_) {
         return null;
     }
 
